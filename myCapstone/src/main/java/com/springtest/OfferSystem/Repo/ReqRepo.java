42801f73984db584fdf35b18cde64b544146a468
mfaisal2020 <88702892+mfaisal2020@users.noreply.github.com>
2022-05-13 18:26:49 -0700

    Delete myCapstone directory

--- myCapstone/src/main/java/com/springtest/OfferSystem/Repo/ReqRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springtest.OfferSystem.Repo;
-
-        import com.springtest.OfferSystem.Models.Request;
-        import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReqRepo extends JpaRepository <Request, Long>{
-}
-
-
